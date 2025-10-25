124400c6432082b3561fe2cd5117b839f7de2a50
Xenofon Zinoviou <xzinoviou@gmail.com>
2025-10-25 18:04:03 +0300

    initial commit

---
+++ src/main/java/com/xzinoviou/myplayground/MyPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.xzinoviou.myplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPlaygroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xzinoviou/myplayground/MyPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xzinoviou.myplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
